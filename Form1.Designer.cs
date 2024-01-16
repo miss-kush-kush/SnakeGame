172a1e4a41f186591ee41f737885340beb2f6661
miss-kush-kush <147203718+miss-kush-kush@users.noreply.github.com>
2024-01-16 23:59:05 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace SnakeGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Play_btn = new System.Windows.Forms.Button();
+            this.Snap_btn = new System.Windows.Forms.Button();
+            this.smallField_btn = new System.Windows.Forms.Button();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.middleField_btn = new System.Windows.Forms.Button();
+            this.bigField_btn = new System.Windows.Forms.Button();
+            this.bigSnake_btn = new System.Windows.Forms.Button();
+            this.middleSnake_btn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.smallSnake_btn = new System.Windows.Forms.Button();
+            this.picCanvas = new System.Windows.Forms.PictureBox();
+            this.growTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Play_btn
+            // 
+            this.Play_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.Play_btn.Location = new System.Drawing.Point(830, 561);
+            this.Play_btn.Name = "Play_btn";
+            this.Play_btn.Size = new System.Drawing.Size(75, 53);
+            this.Play_btn.TabIndex = 0;
+            this.Play_btn.Text = "Play";
+            this.Play_btn.UseVisualStyleBackColor = true;
+            this.Play_btn.Click += new System.EventHandler(this.Play_btn_Click);
+            // 
+            // Snap_btn
+            // 
+            this.Snap_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.Snap_btn.Location = new System.Drawing.Point(943, 561);
+            this.Snap_btn.Name = "Snap_btn";
+            this.Snap_btn.Size = new System.Drawing.Size(75, 53);
+            this.Snap_btn.TabIndex = 1;
+            this.Snap_btn.Text = "Snap";
+            this.Snap_btn.UseVisualStyleBackColor = true;
+            this.Snap_btn.Click += new System.EventHandler(this.Snap_btn_Click);
+            // 
+            // smallField_btn
+            // 
+            this.smallField_btn.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.smallField_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.smallField_btn.Location = new System.Drawing.Point(830, 65);
+            this.smallField_btn.Name = "smallField_btn";
+            this.smallField_btn.Size = new System.Drawing.Size(100, 40);
+            this.smallField_btn.TabIndex = 2;
+            this.smallField_btn.Text = "Small";
+            this.smallField_btn.UseVisualStyleBackColor = true;
+            this.smallField_btn.Click += new System.EventHandler(this.smallField_btn_Click);
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 50;
+            this.gameTimer.Tick += new System.EventHandler(this.GameTimerEvent);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label1.Location = new System.Drawing.Point(825, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 26);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Pick field size";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(22, 579);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(770, 35);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // middleField_btn
+            // 
+            this.middleField_btn.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.middleField_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.middleField_btn.Location = new System.Drawing.Point(830, 132);
+            this.middleField_btn.Name = "middleField_btn";
+            this.middleField_btn.Size = new System.Drawing.Size(100, 44);
+            this.middleField_btn.TabIndex = 5;
+            this.middleField_btn.Text = "Middle";
+            this.middleField_btn.UseVisualStyleBackColor = true;
+            this.middleField_btn.Click += new System.EventHandler(this.middleField_btn_Click);
+            // 
+            // bigField_btn
+            // 
+            this.bigField_btn.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.bigField_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.bigField_btn.Location = new System.Drawing.Point(830, 201);
+            this.bigField_btn.Name = "bigField_btn";
+            this.bigField_btn.Size = new System.Drawing.Size(100, 44);
+            this.bigField_btn.TabIndex = 6;
+            this.bigField_btn.Text = "Big";
+            this.bigField_btn.UseVisualStyleBackColor = true;
+            this.bigField_btn.Click += new System.EventHandler(this.bigField_btn_Click);
+            // 
+            // bigSnake_btn
+            // 
+            this.bigSnake_btn.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.bigSnake_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.bigSnake_btn.Location = new System.Drawing.Point(830, 476);
+            this.bigSnake_btn.Name = "bigSnake_btn";
+            this.bigSnake_btn.Size = new System.Drawing.Size(100, 48);
+            this.bigSnake_btn.TabIndex = 10;
+            this.bigSnake_btn.Text = "Big";
+            this.bigSnake_btn.UseVisualStyleBackColor = true;
+            this.bigSnake_btn.Click += new System.EventHandler(this.bigSnake_btn_Click);
+            // 
+            // middleSnake_btn
+            // 
+            this.middleSnake_btn.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.middleSnake_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.middleSnake_btn.Location = new System.Drawing.Point(830, 407);
+            this.middleSnake_btn.Name = "middleSnake_btn";
+            this.middleSnake_btn.Size = new System.Drawing.Size(100, 46);
+            this.middleSnake_btn.TabIndex = 9;
+            this.middleSnake_btn.Text = "Middle";
+            this.middleSnake_btn.UseVisualStyleBackColor = true;
+            this.middleSnake_btn.Click += new System.EventHandler(this.middleSnake_btn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label2.Location = new System.Drawing.Point(825, 300);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 26);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Pick snake size";
+            // 
+            // smallSnake_btn
+            // 
+            this.smallSnake_btn.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.smallSnake_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.smallSnake_btn.Location = new System.Drawing.Point(830, 340);
+            this.smallSnake_btn.Name = "smallSnake_btn";
+            this.smallSnake_btn.Size = new System.Drawing.Size(100, 46);
+            this.smallSnake_btn.TabIndex = 7;
+            this.smallSnake_btn.Text = "Small";
+            this.smallSnake_btn.UseVisualStyleBackColor = true;
+            this.smallSnake_btn.Click += new System.EventHandler(this.smallSnake_btn_Click);
+            // 
+            // picCanvas
+            // 
+            this.picCanvas.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.picCanvas.Location = new System.Drawing.Point(22, 25);
+            this.picCanvas.Name = "picCanvas";
+            this.picCanvas.Size = new System.Drawing.Size(770, 522);
+            this.picCanvas.TabIndex = 11;
+            this.picCanvas.TabStop = false;
+            this.picCanvas.Paint += new System.Windows.Forms.PaintEventHandler(this.UpdatePictureBoxGraphics);
+            // 
+            // growTimer
+            // 
+            this.growTimer.Interval = 1500;
+            this.growTimer.Tick += new System.EventHandler(this.GrowTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1100, 644);
+            this.Controls.Add(this.picCanvas);
+            this.Controls.Add(this.bigSnake_btn);
+            this.Controls.Add(this.middleSnake_btn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.smallSnake_btn);
+            this.Controls.Add(this.bigField_btn);
+            this.Controls.Add(this.middleField_btn);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.smallField_btn);
+            this.Controls.Add(this.Snap_btn);
+            this.Controls.Add(this.Play_btn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Play_btn;
+        private System.Windows.Forms.Button Snap_btn;
+        private System.Windows.Forms.Button smallField_btn;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button middleField_btn;
+        private System.Windows.Forms.Button bigField_btn;
+        private System.Windows.Forms.Button bigSnake_btn;
+        private System.Windows.Forms.Button middleSnake_btn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button smallSnake_btn;
+        private System.Windows.Forms.PictureBox picCanvas;
+        private System.Windows.Forms.Timer growTimer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Diagnostics;
+
+namespace SnakeGame
+{
+    public partial class Form1 : Form
+    {
+        private List<Circle> Snake = new List<Circle>();
+        private int maxWidth;
+        private int maxHeight;
+        private int snakeSizeTarget;
+        bool goLeft, goRight, goDown, goUp;
+
+        public Form1()
+        {
+            InitializeComponent();
+            new Settings();
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left && Settings.directions != "right")
+            {
+                goLeft = true;
+            }
+            if (e.KeyCode == Keys.Right && Settings.directions != "left")
+            {
+                goRight = true;
+            }
+            if (e.KeyCode == Keys.Up && Settings.directions != "down")
+            {
+                goUp = true;
+            }
+            if (e.KeyCode == Keys.Down && Settings.directions != "up")
+            {
+                goDown = true;
+            }
+
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = false;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = false;
+            }
+        }
+
+        private void Play_btn_Click(object sender, EventArgs e)
+        {
+            RestartGame();
+        }
+
+        private void Snap_btn_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.FileName = "Snake Game SnapShot MOO ICT";
+            dialog.DefaultExt = "jpg";
+            dialog.Filter = "JPG Image File | *.jpg";
+            dialog.ValidateNames = true;
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                int width = Convert.ToInt32(picCanvas.Width);
+                int height = Convert.ToInt32(picCanvas.Height);
+                Bitmap bmp = new Bitmap(width, height);
+                picCanvas.DrawToBitmap(bmp, new Rectangle(0, 0, width, height));
+                bmp.Save(dialog.FileName, ImageFormat.Jpeg);
+            }
+
+        }
+
+        private void InitializeGameSettings(int fieldSize, int snakeSize)
+        {
+            picCanvas.Size = new Size(fieldSize, fieldSize-100);
+            snakeSizeTarget = snakeSize;
+        }
+
+        private void smallField_btn_Click(object sender, EventArgs e)
+        {
+            InitializeGameSettings(200, snakeSizeTarget);
+        }
+
+        private void middleField_btn_Click(object sender, EventArgs e)
+        {
+            InitializeGameSettings(350, snakeSizeTarget);
+        }
+
+        private void bigField_btn_Click(object sender, EventArgs e)
+        {
+            InitializeGameSettings(450, snakeSizeTarget);
+        }
+
+        private void smallSnake_btn_Click(object sender, EventArgs e)
+        {
+            snakeSizeTarget = 20;
+        }
+
+        private void middleSnake_btn_Click(object sender, EventArgs e)
+        {
+            snakeSizeTarget = 40;
+        }
+
+        private void bigSnake_btn_Click(object sender, EventArgs e)
+        {
+            snakeSizeTarget = 50;
+        }
+
+        private void GameTimerEvent(object sender, EventArgs e)
+        {
+            if (goLeft)
+            {
+                Settings.directions = "left";
+            }
+            if (goRight)
+            {
+                Settings.directions = "right";
+            }
+            if (goDown)
+            {
+                Settings.directions = "down";
+            }
+            if (goUp)
+            {
+                Settings.directions = "up";
+            }
+
+            CheckForCollision();
+
+            if (gameTimer.Enabled)
+            {
+                MoveSnake();
+            }
+            picCanvas.Invalidate();
+
+        }
+
+        private void MoveSnake()
+        {
+            for (int i = Snake.Count - 1; i >= 0; i--)
+            {
+                if (i == 0)
+                {
+                    switch (Settings.directions)
+                    {
+                        case "left":
+                            Snake[i].X--;
+                            break;
+                        case "right":
+                            Snake[i].X++;
+                            break;
+                        case "down":
+                            Snake[i].Y++;
+                            break;
+                        case "up":
+                            Snake[i].Y--;
+                            break;
+                    }
+
+                }
+                else
+                {
+                    Snake[i].X = Snake[i - 1].X;
+                    Snake[i].Y = Snake[i - 1].Y;
+                }
+            }
+        }
+
+        private void UpdatePictureBoxGraphics(object sender, PaintEventArgs e)
+        {
+            Graphics canvas = e.Graphics;
+
+            Brush snakeColour;
+
+            for (int i = 0; i < Snake.Count; i++)
+            {
+                if (i == 0)
+                {
+                    snakeColour = Brushes.Black;
+                }
+                else
+                {
+                    snakeColour = Brushes.DarkGreen;
+                }
+
+                canvas.FillEllipse(snakeColour, new Rectangle
+                    (
+                    Snake[i].X * Settings.Width,
+                    Snake[i].Y * Settings.Height,
+                    Settings.Width, Settings.Height
+                    ));
+            }
+
+        }
+
+        private void GrowTimerEvent(object sender, EventArgs e)
+        {
+            IncreaseSize();
+            UpdateProgressBar();
+        }
+
+        private void RestartGame()
+        {
+            maxWidth = picCanvas.Width / Settings.Width-1;
+            maxHeight = picCanvas.Height / Settings.Height-1;
+
+            progressBar1.Maximum = snakeSizeTarget;
+
+            Snake.Clear();
+
+            Play_btn.Enabled = false;
+            Snap_btn.Enabled = false;
+            smallField_btn.Enabled = false;
+            middleField_btn.Enabled = false;
+            bigField_btn.Enabled = false;
+            smallSnake_btn.Enabled = false;
+            middleSnake_btn.Enabled = false;
+            bigSnake_btn.Enabled = false;
+
+            Circle head = new Circle { X = 10, Y = 5 };
+            Snake.Add(head); // adding the head part of the snake to the list
+
+            for (int i = 0; i < 10; i++)
+            {
+                Circle body = new Circle();
+                Snake.Add(body);
+            }
+
+            gameTimer.Start();
+            growTimer.Start();
+
+        }
+
+        private void IncreaseSize()
+        {
+            Circle body = new Circle
+            {
+                X = Snake[Snake.Count - 1].X,
+                Y = Snake[Snake.Count - 1].Y
+            };
+
+            Snake.Add(body);
+
+        }
+
+        private void UpdateProgressBar()
+        {
+            progressBar1.Value = Math.Min(progressBar1.Maximum, Snake.Count - 10);
+            if (progressBar1.Value >= progressBar1.Maximum)
+            {
+                YouWin();
+            }
+        }
+
+        private void StopGame()
+        {
+            gameTimer.Stop();
+            growTimer.Stop();
+            Play_btn.Enabled = true;
+            Snap_btn.Enabled = true;
+            smallField_btn.Enabled = true;
+            middleField_btn.Enabled = true;
+            bigField_btn.Enabled = true;
+            smallSnake_btn.Enabled = true;
+            middleSnake_btn.Enabled = true;
+            bigSnake_btn.Enabled = true;
+        }
+
+        private void YouWin()
+        {
+            StopGame();
+            MessageBox.Show("You Win!");
+        }
+
+        private void GameOver()
+        {
+            StopGame();
+            MessageBox.Show("Game Over!");
+        }
+
+        private void CheckForCollision()
+        {
+            // Check if snake hits the borders
+            if (Snake[0].X <= 0 || Snake[0].X >= maxWidth || Snake[0].Y <= 0 || Snake[0].Y >= maxHeight)
+            {
+                GameOver();
+            }
+
+
+            // Check if snake hits itself
+            for (int i = 1; i < Snake.Count; i++)
+            {
+                if (Snake[0].X == Snake[i].X && Snake[0].Y == Snake[i].Y)
+                {
+                    GameOver();
+                }
+            }
+        }
+    }
+}
